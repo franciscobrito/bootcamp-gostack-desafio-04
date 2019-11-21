4fde49405196ae1a6ea911160ba8a25315869bc7
franciscobrito <francisco.brito.ac@gmail.com>
2019-11-21 17:59:57 -0300

    Primeiro commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function components() {
+  return (
+    <header>
+      <nav>
+        <img src="https://i.imgur.com/KDIDiSE.png" alt="Facebook" />
+        <div>
+          <span>Meu prefil</span>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  );
+}
